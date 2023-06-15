dadee9e41ed911aaee482c405d65778ea59bec6f
Calisthetic <vovakalash04@mail.ru>
2023-06-15 21:50:14 +0500

    cool categories

--- cats-react/src/pages/CheckCats.js
+++ cats-react/src/pages/CheckCats.js
@@ -1,4 +1,4 @@
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 const apiUrl = "https://localhost:7288/api/"
 
@@ -9,21 +9,20 @@ const CheckCats = () => {
   const catIdRef = useRef();
   
   const [currentCatImage, setCurrentCatImage] = useState(apiUrl + "Cats/" + currentCatId.current + ".jpeg");
-  const [catInfoData, setCatInfoData] = useState();
-  //const [isUpdate, setIsUpdate] = useState(false)
+  const [catInfoData, setCatInfoData] = useState([{}]);
 
   const [categoriesData, setCategoriesData] = useState([{}])
   useEffect(() => {
-    fetch("Classifications/ctg" + localStorage.getItem("userID"), {method: 'GET'})
+    fetch(apiUrl + "Classifications/ctg", {method: 'GET'})
     .then(response => response.json())
-    .then(data => setCategoriesData(data))
+    .then(data => {let obg = {active: false} ;data.map((item) => item = Object.assign(item, obg)) ;setCategoriesData(data)})
   }, []);
 
   async function RefreshCat() {
     await fetch(apiUrl + "Cats/Info/" + currentCatId.current, {
       method: 'GET',
     }).then(response => response.json())
-      .then(json => setCatInfoData(json))
+      .then(json => {setCatInfoData(json)})
       .catch(error => console.log('Failed: ' + error.message));
     currentImageSrc = apiUrl + "Cats/" + currentCatId.current + ".jpeg"
     setCurrentCatImage(currentImageSrc)
@@ -34,16 +33,46 @@ const CheckCats = () => {
       RefreshCat()
     }, 500);
   }
+  function OnCategoryClick(e) {
+    let ctg_id = parseInt(e.target.id.split('_')[1])
+    let currentCategoryId
+    for (let i = 0; i < categoriesData.length; i++) {
+      if (categoriesData[i].category_id === ctg_id) {
+        currentCategoryId = i
+        break
+      }
+    }
+    if (categoriesData[currentCategoryId].active === true) {
+      document.getElementById("category_" + categoriesData[currentCategoryId].category_id).style.backgroundColor = "rgb(100 116 139)"
+      categoriesData[currentCategoryId].active = false
+    } else {
+      document.getElementById("category_" + categoriesData[currentCategoryId].category_id).style.backgroundColor = "rgb(71 85 105)"
+      categoriesData[currentCategoryId].active = true
+    }
+    console.log(e.target.id.split('_')[1])
+  }
   function NextCat() {
     currentCatId.current = (parseInt(currentCatId.current) + 1)
     catIdRef.current.value = currentCatId.current
     RefreshCat()
+    ClearCategories()
   }
   function PreviousCat() {
     currentCatId.current = (parseInt(currentCatId.current) - 1)
-    console.log(currentCatId.current)
     catIdRef.current.value = currentCatId.current
     RefreshCat()
+    ClearCategories()
+  }
+  function ClearCategories() {
+    for (let i = 0; i < categoriesData.length; i++) {
+      if (categoriesData[i].active === true) {
+        document.getElementById("category_" + categoriesData[i].category_id).style.backgroundColor = "rgb(100 116 139)"
+        categoriesData[i].active = false
+      }
+    }
+  }
+  function DeleteCat() {
+    console.log(categoriesData)
   }
 
   return (
@@ -55,17 +84,17 @@ const CheckCats = () => {
           <img src={currentCatImage} alt="cat" className=" lg:max-h-100 w-full lg:w-auto"></img>
         </div>
         <div className="bg-slate-500 lg:overflow-y-auto ">
-        {(typeof categoriesData[0] == "undefined") ? (
+          {(typeof categoriesData[0] == "undefined") ? (
             <p>Loading...</p>
             ) : (
               categoriesData.map((item, index) => (
-                <div key={index}>
+                <div id={"category_" + item.category_id} data-active="0" onClick={OnCategoryClick} className=" w-full h-8 flex justify-center items-center hover:bg-slate-600 transition-all cursor-pointer" key={index}>
                   {item.name}
                 </div>
               ))
           )}
 
-          <div className="h-96">sus</div>
+          <div className="h-96">{catInfoData === undefined ? "NoData" : catInfoData.age}</div>
           <div className="h-96">sus</div>
           <div className="h-96">sus</div>
           <div>sus</div>
@@ -76,7 +105,7 @@ const CheckCats = () => {
           <svg aria-hidden="true" className="w-5 h-5 mr-2" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M7.707 14.707a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 1.414L5.414 9H17a1 1 0 110 2H5.414l2.293 2.293a1 1 0 010 1.414z"></path></svg>
           Previous
         </div>
-        <div className="inline-flex items-center cursor-pointer px-2 sm:px-4 py-2 text-sm font-medium text-gray-500 bg-white border border-gray-300 rounded-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
+        <div onClick={DeleteCat} className="inline-flex items-center cursor-pointer px-2 sm:px-4 py-2 text-sm font-medium text-gray-500 bg-white border border-gray-300 rounded-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
           <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#ff0000" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><polyline points="3 6 5 6 21 6"></polyline><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path><line x1="10" y1="11" x2="10" y2="17"></line><line x1="14" y1="11" x2="14" y2="17"></line></svg>
         </div>
         <div className="inline-flex items-center cursor-pointer px-2 sm:px-4 py-2 text-sm font-medium text-gray-500 bg-white border border-gray-300 rounded-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
